071e29ef58af0934a73c5ee5b876710e9e1a2988
Ioopus <moodie.allam@gmail.com>
2023-01-29 22:30:37 -0500

    lab 2.2 completed

--- app/src/main/java/com/example/myapplication/MainActivityViewModel.java
+++ app/src/main/java/com/example/myapplication/MainActivityViewModel.java
@@ -4,8 +4,5 @@ import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 public class MainActivityViewModel extends ViewModel {
-
-    //Holds a string , but notifies observers when changed
-    public MutableLiveData<String> editTextContents = new MutableLiveData<>();
     public MutableLiveData<Boolean> isSelected = new MutableLiveData<>(false);
 }
